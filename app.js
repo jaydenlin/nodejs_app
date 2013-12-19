c4fee6d4ad44f16d8fd00e54238b2ebe057b8a76
root <root@ip-10-128-193-132.ap-southeast-1.compute.internal>
2013-12-19 01:02:08 +0000

    remote contrl

--- app.js
+++ app.js
@@ -9,23 +9,33 @@ var express = require('express'),
   app.get('/', function (req, res) {
 	  res.sendfile(__dirname + '/remote.html');
   });
+  
+  app.get('/test', function (req, res) {
+          res.sendfile(__dirname + '/test.html');
+  });
 
   io.sockets.on('connection', function (socket) {
 	  socket.emit('news', { hello: 'world' });
-	  socket.on('my other event', function (data) {
-		      console.log(data);
-			    });
 
 	  socket.on('remote:selectAnswer',function(data){
-	        socket.broadcast.emit('server:receiveAnswer', data);
-
+	        //socket.broadcast.emit('server:receiveAnswer', data);
+                io.sockets.emit('server:receiveAnswer', data);
   	  });
 
   	  socket.on('device:questionDone',function(data){
-		socket.broadcast.emit('server:prepareAnswers', JSON.parse(data));
-	        console.log(JSON.parse(data));
+		//socket.broadcast.emit('server:prepareAnswers', JSON.parse(data));
+		io.sockets.emit('server:prepareAnswers', JSON.parse(data));
+		console.log(JSON.parse(data));
 
   	  });
+
+          socket.on('remote:requestAnswers', function(data){
+                //socket.broadcast.emit('server:requestAnswers', {});
+                io.sockets.emit('server:requestAnswers', {});
+		//debug
+		//socket.emit('server:requestAnswers', {});
+		console.log('broadcast server:requestAnswers');
+          });
 
           //socket.emit('server:prepareAnswers', {'preparedAnswers':['abc','def']});
 
