5165e5b3d1361d27d0ac816b6f00fa0c96c907c2
root <root@ip-10-128-193-132.ap-southeast-1.compute.internal>
2014-01-17 06:25:31 +0000

    final

--- app.js
+++ app.js
@@ -10,35 +10,33 @@ var express = require('express'),
 	  res.sendfile(__dirname + '/remote.html');
   });
   
-  app.get('/test', function (req, res) {
-          res.sendfile(__dirname + '/test.html');
-  });
 
   io.sockets.on('connection', function (socket) {
+          var room;
 	  socket.emit('news', { hello: 'world' });
 
 	  socket.on('remote:selectAnswer',function(data){
-	        //socket.broadcast.emit('server:receiveAnswer', data);
                 io.sockets.emit('server:receiveAnswer', data);
   	  });
 
   	  socket.on('device:questionDone',function(data){
-		//socket.broadcast.emit('server:prepareAnswers', JSON.parse(data));
 		io.sockets.emit('server:prepareAnswers', JSON.parse(data));
 		console.log(JSON.parse(data));
 
   	  });
 
           socket.on('remote:requestAnswers', function(data){
-                //socket.broadcast.emit('server:requestAnswers', {});
                 io.sockets.emit('server:requestAnswers', {});
-		//debug
-		//socket.emit('server:requestAnswers', {});
 		console.log('broadcast server:requestAnswers');
           });
 
+
           //socket.emit('server:prepareAnswers', {'preparedAnswers':['abc','def']});
 
-  });
+  
+    });
+
+
+
 
 
---
+++ app1.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+    app = express(),
+	http = require('http'),
+	server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+  server.listen(8081);
+
+  app.get('/', function (req, res) {
+	  res.sendfile(__dirname + '/remote1.html');
+  });
+  
+
+  io.sockets.on('connection', function (socket) {
+          var room;
+	  socket.emit('news', { hello: 'world' });
+
+	  socket.on('remote:selectAnswer',function(data){
+                io.sockets.emit('server:receiveAnswer', data);
+  	  });
+
+  	  socket.on('device:questionDone',function(data){
+		io.sockets.emit('server:prepareAnswers', JSON.parse(data));
+		console.log(JSON.parse(data));
+
+  	  });
+
+          socket.on('remote:requestAnswers', function(data){
+                io.sockets.emit('server:requestAnswers', {});
+		console.log('broadcast server:requestAnswers');
+          });
+
+
+          //socket.emit('server:prepareAnswers', {'preparedAnswers':['abc','def']});
+
+  
+    });
+
+
+
+
+
